b05dd3deea06424080435aa632b0db8f4851666e
denimar <denimar@gmail.com>
2016-06-03 03:38:48 -0300

    Examples Under Construction

--- examples/options/script.js
+++ examples/options/script.js
@@ -2,17 +2,10 @@
 angular.module('myApp', ['ui-deni-grid']);
 
 //Controller
-angular.module('myApp').controller('SimpleCtrl', function($scope, $http) {
+angular.module('myApp').controller('ExampleCtrl', function($scope, $http) {
 
     $scope.gridOptions = {
-        idField: 'id',
-        rowHeight: '22px', //22px is default
-        hideHeaders: false, //false is default  
-        stripRows: true, //true is default       
-        enableColumnResize: true, //true is default
-        sortableColumns: true, //true is default
-        //multiSelect: true, //default is false 
-        url: '../../../data/employees/employees-00500.json',
+        url: 'http://denimar.github.io/static-data/employees/00500.json',
         columns: [
             { 
                 header:'Name', 
