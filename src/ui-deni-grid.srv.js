ac7615705921b22405988378f471af1ab4bf0d0c
denimar <denimar@gmail.com>
2016-10-04 14:49:49 -0300

    ...

--- src/ui-deni-grid.srv.js
+++ src/ui-deni-grid.srv.js
@@ -2320,18 +2320,6 @@ function xml2json(xml, tab) {
 	 *
 	 */
 	me.loadData = function(controller, data) {
-		if (data.length > 0) {
-			controller.bodyViewport.css({
-				'overflow-x': 'auto',
-				'overflow-y': 'scroll'				
-			});
-		} else {
-			controller.bodyViewport.css({
-				'overflow-x': 'hidden',
-				'overflow-y': 'hidden'				
-			});
-		}
-
 		///////////////////////////////////////////////////////////////////////////
 		//BeforeLoad Event
 		///////////////////////////////////////////////////////////////////////////
@@ -2525,6 +2513,18 @@ function xml2json(xml, tab) {
 		}
 		///////////////////////////////////////////////////////////////////////////
 		///////////////////////////////////////////////////////////////////////////
+
+		if (data.length > 0) {
+			controller.bodyViewport.css({
+				'overflow-x': 'auto',
+				'overflow-y': 'scroll'				
+			});
+		} else {
+			controller.bodyViewport.css({
+				'overflow-x': 'hidden',
+				'overflow-y': 'hidden'				
+			});
+		}		
 	};
 
 
