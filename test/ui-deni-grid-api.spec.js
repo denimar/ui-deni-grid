3aaefb7d06a0a040e6becc1da3698d7978856572
denimar <denimar@gmail.com>
2016-12-22 11:23:22 -0200

    testing...

--- test/ui-deni-grid-api.spec.js
+++ test/ui-deni-grid-api.spec.js
@@ -1,7 +1,7 @@
 describe('ui-deni-grid API', function() {
 
   beforeEach(function() {
-    browser.get('http://localhost:3001/examples/api-usage/index.html');
+    browser.get('https://denimar.github.io/ui-deni-grid/examples/usability/api-usage/');
   });
 
   it('should test (load, selectRow, getSelectedRow)', function() {
