293dbc5dcfb2e193752a6032461b8d27bd020a31
denimar <denimar@gmail.com>
2016-12-22 13:00:03 -0200

    testing...

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,6 +1,4 @@
 exports.config = {
-  sauceUser: 'denimar',
-  sauceKey: 'dm90460',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./test/**/*.spec.js'],
   /*
@@ -60,3 +58,13 @@ exports.config = {
   }
 
 };
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
+}
