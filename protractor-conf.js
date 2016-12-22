35ab363bafc39f42ef9eb61b61525ea247227fca
denimar <denimar@gmail.com>
2016-12-22 21:09:16 -0200

    testing...

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,6 +1,53 @@
 var config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  sauceSeleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./test/**/*.spec.js'],
+  sauceUser: 'denimar',
+  sauceKey: '6bda91d2-ca2c-4ea0-bf43-4c2a891118f9',
+  // Capabilities to be passed to the webdriver instance.
+  // This option is called "capabilities" in the protractor docs
+  // but whatever.  this also works.
+  multiCapabilities: [{
+      // by default, these first two browsers will come up in
+      // Linux if you don't specify an OS
+      'name': 'Chrome',
+      'browserName': 'chrome'
+  }, {
+      'name': 'Firefox',
+      'browserName': 'firefox'
+  }, {
+      'name': 'Win XP/IE8',
+      'os': 'Windows XP',
+      'browserName': 'internet explorer',
+      'version': '8.0'
+  }, {
+      'name': 'Win7/IE8',
+      'os': 'Windows 7',
+      'browserName': 'internet explorer',
+      'version': '8.0'
+  }, {
+      'name': 'Win7/IE9',
+      'os': 'Windows 7',
+      'browserName': 'internet explorer',
+      'version': '9.0'
+  }, {
+      'name': 'Win8/IE10',
+      'os': 'Windows 8',
+      'browserName': 'internet explorer',
+      'version': '10.0'
+  }, {
+      'name': 'Win8.1/IE11',
+      'os': 'Windows 8.1',
+      'browserName': 'internet explorer',
+      'version': '11.0'
+  }],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+      showColors: true,
+      defaultTimeoutInterval: 30000
+  },
+
   /*
   files: [
     './bower_components/angular/angular.js',
