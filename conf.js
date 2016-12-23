ea6fc68d1ea8dca473a415dac9608964dcec7472
denimar <denimar@gmail.com>
2016-12-23 16:06:49 -0200

    ....

--- conf.js
+++ conf.js
@@ -2,6 +2,7 @@
 exports.config = {
   sauceUser: 'ui-deni-grid',
   sauceKey: '6b220e08-e488-43c0-982d-b76e0e4b9170',
+  sauceLabs.build
 
   //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
 
@@ -12,61 +13,64 @@ exports.config = {
       // by default, these first two browsers will come up in
       // Linux if you don't specify an OS
       'name': 'Linux / Chrome',
-      'browserName': 'chrome'
-    },
-    {
-      'name': 'Linux / Firefox',
-      'browserName': 'firefox'
-    },
-    // { DOESN'T WORK
-    //   'name': 'Win XP/IE8',
-    //   'os': 'Windows XP',
-    //   'browserName': 'internet explorer',
-    //   'version': '8.0'
-    // },
-    // { DOESN'T WORK
-    //   'name': 'Win7/IE8',
-    //   'os': 'Windows 7',
-    //   'browserName': 'internet explorer',
-    //   'version': '8.0'
-    // },
-    {
-      'name': 'Win7 / IE9',
-      'platform': 'Windows 7',
-      'browserName': 'internet explorer',
-      'version': '9.0'
-    },
-    {
-      'name': 'Win8 / IE10',
-      'platform': 'Windows 8',
-      'browserName': 'internet explorer',
-      'version': '10.0'
-    },
-    {
-      'name': 'Win8.1 / IE11',
-      'platform': 'Windows 8.1',
-      'browserName': 'internet explorer',
-      'version': '11.0'
-    },
-    {
-      'name': 'Win10 / Edge 14',
-      'platform': 'Windows 10',
-      'browserName': 'microsoftedge',
-      'version': '14'
-    },
-    {
-      'name': 'Mac10.8 / Chrome 49.0',
       'browserName': 'chrome',
-      'platform': 'Mac 10.8',
-      'version': '49.0'
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      'build': process.env.TRAVIS_BUILD_NUMBER
     },
-    // { DOESN'T WORK
-    //   'name': 'Mac10.8 / Firefox 48.0',
-    //   'browserName': 'firefox',
+    //
+    // {
+    //   'name': 'Linux / Firefox',
+    //   'browserName': 'firefox'
+    // },
+    // // { DOESN'T WORK
+    // //   'name': 'Win XP/IE8',
+    // //   'os': 'Windows XP',
+    // //   'browserName': 'internet explorer',
+    // //   'version': '8.0'
+    // // },
+    // // { DOESN'T WORK
+    // //   'name': 'Win7/IE8',
+    // //   'os': 'Windows 7',
+    // //   'browserName': 'internet explorer',
+    // //   'version': '8.0'
+    // // },
+    // {
+    //   'name': 'Win7 / IE9',
+    //   'platform': 'Windows 7',
+    //   'browserName': 'internet explorer',
+    //   'version': '9.0'
+    // },
+    // {
+    //   'name': 'Win8 / IE10',
+    //   'platform': 'Windows 8',
+    //   'browserName': 'internet explorer',
+    //   'version': '10.0'
+    // },
+    // {
+    //   'name': 'Win8.1 / IE11',
+    //   'platform': 'Windows 8.1',
+    //   'browserName': 'internet explorer',
+    //   'version': '11.0'
+    // },
+    // {
+    //   'name': 'Win10 / Edge 14',
+    //   'platform': 'Windows 10',
+    //   'browserName': 'microsoftedge',
+    //   'version': '14'
+    // },
+    // {
+    //   'name': 'Mac10.8 / Chrome 49.0',
+    //   'browserName': 'chrome',
     //   'platform': 'Mac 10.8',
-    //   'version': '48.0'
-    // }
-
+    //   'version': '49.0'
+    // },
+    // // { DOESN'T WORK
+    // //   'name': 'Mac10.8 / Firefox 48.0',
+    // //   'browserName': 'firefox',
+    // //   'platform': 'Mac 10.8',
+    // //   'version': '48.0'
+    // // }
+    //
   ],
 
 
