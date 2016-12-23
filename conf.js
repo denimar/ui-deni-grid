c8461750405602b7891ae95f7e7d691c6a438c08
denimar <denimar@gmail.com>
2016-12-23 16:33:01 -0200

    ....

--- conf.js
+++ conf.js
@@ -19,63 +19,68 @@ exports.config = {
       // Linux if you don't specify an OS
       'name': 'Linux / Chrome',
       'browserName': 'chrome',
-      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER,
-      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
     },
-    //
-    // {
-    //   'name': 'Linux / Firefox',
-    //   'browserName': 'firefox'
-    // },
-    // // { DOESN'T WORK
-    // //   'name': 'Win XP/IE8',
-    // //   'os': 'Windows XP',
-    // //   'browserName': 'internet explorer',
-    // //   'version': '8.0'
-    // // },
-    // // { DOESN'T WORK
-    // //   'name': 'Win7/IE8',
-    // //   'os': 'Windows 7',
-    // //   'browserName': 'internet explorer',
-    // //   'version': '8.0'
-    // // },
-    // {
-    //   'name': 'Win7 / IE9',
-    //   'platform': 'Windows 7',
+
+    {
+      'name': 'Linux / Firefox',
+      'browserName': 'firefox',
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
+    },
+    // { DOESN'T WORK
+    //   'name': 'Win XP/IE8',
+    //   'os': 'Windows XP',
     //   'browserName': 'internet explorer',
-    //   'version': '9.0'
+    //   'version': '8.0'
     // },
-    // {
-    //   'name': 'Win8 / IE10',
-    //   'platform': 'Windows 8',
+    // { DOESN'T WORK
+    //   'name': 'Win7/IE8',
+    //   'os': 'Windows 7',
     //   'browserName': 'internet explorer',
-    //   'version': '10.0'
+    //   'version': '8.0'
     // },
-    // {
-    //   'name': 'Win8.1 / IE11',
-    //   'platform': 'Windows 8.1',
-    //   'browserName': 'internet explorer',
-    //   'version': '11.0'
-    // },
-    // {
-    //   'name': 'Win10 / Edge 14',
-    //   'platform': 'Windows 10',
-    //   'browserName': 'microsoftedge',
-    //   'version': '14'
-    // },
-    // {
-    //   'name': 'Mac10.8 / Chrome 49.0',
-    //   'browserName': 'chrome',
+    {
+      'name': 'Win7 / IE9',
+      'platform': 'Windows 7',
+      'browserName': 'internet explorer',
+      'version': '9.0',
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
+    },
+    {
+      'name': 'Win8 / IE10',
+      'platform': 'Windows 8',
+      'browserName': 'internet explorer',
+      'version': '10.0',
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
+    },
+    {
+      'name': 'Win8.1 / IE11',
+      'platform': 'Windows 8.1',
+      'browserName': 'internet explorer',
+      'version': '11.0',
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
+    },
+    {
+      'name': 'Win10 / Edge 14',
+      'platform': 'Windows 10',
+      'browserName': 'microsoftedge',
+      'version': '14',
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
+    },
+    {
+      'name': 'Mac10.8 / Chrome 49.0',
+      'browserName': 'chrome',
+      'platform': 'Mac 10.8',
+      'version': '49.0',
+      'build': 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
+    },
+    // { DOESN'T WORK
+    //   'name': 'Mac10.8 / Firefox 48.0',
+    //   'browserName': 'firefox',
     //   'platform': 'Mac 10.8',
-    //   'version': '49.0'
-    // },
-    // // { DOESN'T WORK
-    // //   'name': 'Mac10.8 / Firefox 48.0',
-    // //   'browserName': 'firefox',
-    // //   'platform': 'Mac 10.8',
-    // //   'version': '48.0'
-    // // }
-    //
+    //   'version': '48.0'
+    // }
+
   ],
 
 
