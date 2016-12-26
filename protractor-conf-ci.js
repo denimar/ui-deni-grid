e8c885cb084ff95e9371927e8015bca8e72e4458
denimar <denimar@gmail.com>
2016-12-26 17:00:38 -0200

    ....

--- protractor-conf-ci.js
+++ protractor-conf-ci.js
@@ -3,8 +3,10 @@ exports.config = {
   sauceUser: 'ui-deni-grid',
   sauceKey: '6b220e08-e488-43c0-982d-b76e0e4b9170',
 
-  specs: ['test/*spec.js'],
-
+  specs: ['test/**/*.spec.js'],
+  onPrepare: () => {
+    browser.driver.manage().window().maximize();
+  },
   multiCapabilities: [
     {
       // by default, these first two browsers will come up in
--- protractor-conf.js
+++ protractor-conf.js
@@ -1,8 +1,24 @@
 
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/*spec.js'],
+  //specs: ['test/**/*.spec.js'],
+  specs: ['test/basic/*.spec.js'],
+
+  files: [
+    './bower_components/jquery/dist/jquery.min.js'
+  ],
+
   onPrepare: () => {
     browser.driver.manage().window().maximize();
-  }
+  },
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  },
+
+  //shardTestFiles: true,
+  //maxInstances: 5,
+  //restartBrowserBetweenTests: true
+
 }
