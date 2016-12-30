0f7f7837c0fe6fa33598f03f9b3b9ddf6df70e78
denimar <denimar@gmail.com>
2016-12-30 14:34:28 -0200

    implementing tests

--- test/e2e/protractor-conf-ci.js
+++ test/e2e/protractor-conf-ci.js
@@ -7,9 +7,9 @@ exports.config = {
   specs: ['specs/*.spec.js'],
   //specs: ['test/usability/*.spec.js'],
 
-  onPrepare: () => {
-    browser.driver.manage().window().maximize();
-  },
+  // onPrepare: () => {
+  //   browser.driver.manage().window().maximize();
+  // },
 
   jasmineNodeOpts: {
     showColors: true,
@@ -28,6 +28,33 @@ exports.config = {
   *  http://www.protractortest.org/#/browser-support and https://saucelabs.com/platforms
   *************************************************************************************
   */
-  multiCapabilities: capabilities.getMultiCapabilities()
+  multiCapabilities: capabilities.getMultiCapabilities(),
+
+  framework: 'jasmine2',
+onPrepare: function() {
+    browser.driver.manage().window().maximize();
+    var jasmineReporters = require('jasmine-reporters');
+
+    // returning the promise makes protractor wait for the reporter config before executing tests
+    return browser.getProcessedConfig().then(function(config) {
+        // you could use other properties here if you want, such as platform and version
+        //var browserName = config.capabilities.browserName;
+
+        var junitReporter = new jasmineReporters.TerminalReporter({
+            consolidateAll: true,
+            //savePath: 'testresults',
+            // this will produce distinct xml files for each capability
+            //filePrefix: browserName + '-xmloutput',
+            /*
+            modifySuiteName: function(generatedSuiteName, suite) {
+                // this will produce distinct suite names for each capability,
+                // e.g. 'firefox.login tests' and 'chrome.login tests'
+                return browserName + '.' + generatedSuiteName;
+            }
+            */
+        });
+        jasmine.getEnv().addReporter(junitReporter);
+    });
+},
 
 }
