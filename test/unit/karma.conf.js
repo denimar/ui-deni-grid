12bde0bc16d05240b02cdc95e6a78b5a4cf0fae6
denimar <denimar@gmail.com>
2016-12-30 16:57:31 -0200

    implementing tests...

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -79,6 +79,10 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    onPrepare: () => {
+      console.log('Denimar de Mores Here');
+    }
   })
 }
