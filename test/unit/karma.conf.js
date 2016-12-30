8d0528ef0a99d8e249d428de628516585d3b135f
denimar <denimar@gmail.com>
2016-12-30 12:26:49 -0200

    implementing tests

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -66,8 +66,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'], //to run in travis
-    //browsers: ['Chrome'], //to run in locally
+    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
