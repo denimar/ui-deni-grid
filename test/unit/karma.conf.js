80d990566e21d89105ddf31a863983cc3be53589
denimar <denimar@gmail.com>
2017-01-02 09:30:47 -0200

    implementing code coverage tests

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -38,12 +38,17 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '../../dist/ui-deni-grid.js': 'coverage'
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['narrow', 'jasmine-diff'],
+    reporters: ['narrow', 'jasmine-diff', 'coverage'],
+
+    coverageReporter: {
+      type : 'text'
+    },
 
     narrowReporter: {
         showSuccess: true, //Show success testcase, default is false
@@ -87,7 +92,6 @@ module.exports = function(config) {
     console.log('########################################################################');
     //
   } else {
-    console.log(config.reporters);
     config.reporters.unshift('kjhtml');
   }
 
