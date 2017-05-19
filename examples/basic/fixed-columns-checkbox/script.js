0e30d8b908c56ad71e0db30a9c787787fea7e180
denimar <denimar@gmail.com>
2017-05-19 14:10:04 -0300

    Fixing issue in fixed columns with checkbox

---
+++ examples/basic/fixed-columns-checkbox/script.js
@@ -0,0 +1,43 @@
+//Module
+angular.module('myApp', ['ui-deni-grid']);
+
+//Controller
+angular.module('myApp').controller('ExampleCtrl', function($scope) {
+
+    $scope.gridOptions = {
+        url: 'https://denimar.github.io/static-data/movies/00500.json',
+        fixedCols: {
+            checkbox: true,
+        },
+        columns: [{
+                header: 'Id',
+                name: 'id',
+                width: '90px',
+                align: 'right',
+            }, {
+                header: 'Title',
+                name: 'title',
+                width: '200px',
+                align: 'left',
+            }, {
+                header: 'Popularity',
+                name: 'popularity',
+                width: '120px',
+                align: 'right'
+            }, {
+                header: 'Release',
+                name: 'release_date',
+                width: '130px',
+                align: 'center'
+            }, {
+                header: 'Votes',
+                name: 'vote_count',
+                width: '80px',
+                align: 'center'
+            },
+
+        ]
+
+    };
+
+});
